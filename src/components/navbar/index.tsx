4aed65084e82ea52c33a368728f5d6c95d952a2c
April <huiling_loh_97@hotmail.com>
2023-12-04 08:03:11 +0800

    updated navbar to link to route pages

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,18 +1,22 @@
+import { Link } from 'react-router-dom';
+
 import './style.scss'
 
-const TABS_LIST = [
-  'Home',
-  'Services',
-  'Contact'
-]
+const TABS_LIST = {
+  home: '/',
+  services: '/services',
+  contact: '/contact'
+}
 
 const NavBar = () => {
   return (
     <div className='navbar'>
       <ul className='navbar-list'>
-        {TABS_LIST.map((tab) => {
+        {Object.entries(TABS_LIST).map(([key, value]) => {
           return (
-            <li className='navbar-list-item' key={tab}>{tab}</li>
+            <li className='navbar-list-item' key={key}>
+              <Link to={`${value}`}>{key}</Link>
+            </li>
           )
         })}
       </ul>
