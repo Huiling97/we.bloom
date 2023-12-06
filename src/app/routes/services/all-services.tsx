2d355a8e509e9a12b619cf0d636b119d901f7bec
April <huiling_loh_97@hotmail.com>
2023-12-06 17:27:07 +0800

    updated all services with dummy data

--- src/app/routes/services/all-services.tsx
+++ src/app/routes/services/all-services.tsx
@@ -1,8 +1,29 @@
 import { Outlet } from 'react-router-dom';
+import CardGrid from '../../components/card/card-grid';
 
 const AllServices = () => {
+  const DUMMY_TITLES = [
+    {
+      title: 'Face',
+      image: 'face',
+    },
+    {
+      title: 'Body',
+      image: 'body',
+    },
+    {
+      title: 'Nail',
+      image: 'nail',
+    },
+    {
+      title: 'Hair Removal',
+      image: 'hair-removal',
+    },
+  ];
+
   return (
     <div>
+      <CardGrid type='generic' cards={DUMMY_TITLES} />
       <Outlet />
     </div>
   );
