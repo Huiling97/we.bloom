473e6f5ca51151aa43b3142452441d692d69f2b7
April <huiling_loh_97@hotmail.com>
2023-12-07 18:55:11 +0800

    initialize firebase in app

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.scss';
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
