555b7725717ac268d9fae52c7c6909302ac664bc
April <huiling_loh_97@hotmail.com>
2023-12-12 20:41:55 +0800

    fetch data in service

--- src/app/routes/services/body.tsx
+++ src/app/routes/services/body.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react';
+import { type CardDetailedFormInputProps } from '../../types/card.ts';
+
+import { onValue, ref } from 'firebase/database';
+import { database } from '../../../main.tsx';
+
 const Body = () => {
-  return (
-    <div>Body page</div>
-  )
-}
+  const [services, setServices] = useState<CardDetailedFormInputProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const serviceRef = ref(database, 'services');
+
+    const fetchServicesData = () => {
+      try {
+        onValue(serviceRef, (snapshot) => {
+          if (snapshot) {
+            const data = snapshot.val();
+            if (data) {
+              setServices(data);
+            }
+          } else {
+            throw new Error('Unable to fetch services');
+          }
+          setIsLoading(false);
+        });
+      } catch (e) {
+        setIsLoading(false);
+        throw new Error('Unable to fetch services');
+      }
+    };
+    fetchServicesData();
+  }, []);
+
+  console.log('servies', services);
+
+  return <div>{isLoading ? <div>Loading</div> : <div>Body page</div>}</div>;
+};
 
 export default Body;
