52ebb0efb9b7ed8f9428f7abd18b7b8aec89749a
April <huiling_loh_97@hotmail.com>
2023-12-13 17:22:17 +0800

    fetch categories and services on manage page

--- src/app/components/card/card-overview/index.tsx
+++ src/app/components/card/card-overview/index.tsx
@@ -1,15 +1,25 @@
-import { CardGenericProps } from '../../../types/card.ts';
+import {
+  type CardGenericObjectProps,
+  type CardDetailedFormInputProps,
+  type CardServicesProps,
+} from '../../../types/card.ts';
+import CardDetailed from '../card-detailed/index.tsx';
 
 import './style.scss';
 
-const CardOverview = (data: CardGenericProps) => {
+const displayCategories = (data: CardGenericObjectProps) => {
   return (
     <div className='card-overview-container'>
       {Object.entries(data).map(([key, value]) => {
+        console.log('value', value);
         return (
-          <div key={key}>
+          <div key={key} role='listCategories'>
             <div>{key}</div>
-            <img src={value.imageSrc} className='card-overview-image' />
+            <img
+              src={value.image}
+              alt='category name'
+              className='card-overview-image'
+            />
           </div>
         );
       })}
@@ -17,4 +27,32 @@ const CardOverview = (data: CardGenericProps) => {
   );
 };
 
-export default CardOverview;
+const displayServiceCards = (serviceData: CardDetailedFormInputProps[]) => {
+  return serviceData.map((service, index) => {
+    const { name, description, details } = service;
+    return (
+      <div
+        key={`${name}-${index}`}
+        className='card-overview-detailed-container'
+      >
+        <CardDetailed name={name} description={description} details={details} />
+      </div>
+    );
+  });
+};
+
+const displayServices = (services: CardServicesProps) => {
+  console.log('services', services);
+  return Object.entries(services).map(([key, value], index) => {
+    return (
+      <div key={index}>
+        <div className='card-overview-title'>{key}</div>
+        <div className='card-overview-container'>
+          {displayServiceCards(value)}
+        </div>
+      </div>
+    );
+  });
+};
+
+export { displayCategories, displayServices };
