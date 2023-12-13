b7b8145b7c9fa65502cc64c2c9207feaf62a8d4b
April <huiling_loh_97@hotmail.com>
2023-12-13 17:21:55 +0800

    create generic function for fetching categories

---
+++ src/app/util/fetch-categories.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { type CardGenericObjectProps } from '../../app/types/card.ts';
+
+import { onValue, ref } from 'firebase/database';
+import { database } from '../../main.tsx';
+
+const fetchCategoriesData = () => {
+  const [categoryType, setCategoryType] = useState<string[]>([]);
+  const [categories, setCategories] = useState<CardGenericObjectProps | null>(
+    null
+  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const categoriesRef = ref(database, 'categories');
+
+    try {
+      onValue(categoriesRef, (snapshot) => {
+        if (snapshot) {
+          const data = snapshot.val() as CardGenericObjectProps;
+          if (data) {
+            const updatedTypes = Object.keys(data);
+            setCategoryType(updatedTypes);
+            setCategories(data);
+          }
+        } else {
+          throw new Error('Unable to fetch categories');
+        }
+        setIsLoading(false);
+      });
+    } catch (e) {
+      setIsLoading(false);
+      throw new Error('Unable to fetch categories');
+    }
+  }, []);
+
+  return { isLoading, categories, categoryType };
+};
+
+export default fetchCategoriesData;
