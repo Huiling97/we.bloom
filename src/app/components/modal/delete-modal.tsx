a7a80bd1e5a4fde3dc362da73e238af26eac8fb8
April <huiling_loh_97@hotmail.com>
2023-12-18 18:00:49 +0800

    created delete modal

---
+++ src/app/components/modal/delete-modal.tsx
@@ -0,0 +1,61 @@
+import { useContext } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { type CardGenericObjectProps } from '../../types/card';
+import { type DeleteModalProps } from '../../types/modal.ts';
+import { CategoriesContext } from '../../store/categories-context';
+import { ref, set } from 'firebase/database';
+import { database } from '../../../main.tsx';
+
+const DeleteModal = ({
+  id,
+  showDeleteModal,
+  setShowDeleteModal,
+}: DeleteModalProps) => {
+  const categoriesCtx = useContext(CategoriesContext);
+
+  const handleClose = () => {
+    setShowDeleteModal(false);
+  };
+
+  const getCategoryById = (categories: CardGenericObjectProps, id: string) => {
+    return Object.values(categories).find((category) => category.id === id);
+  };
+
+  const onDeleteCategoryHandler = () => {
+    if (categoriesCtx.categories) {
+      const selectedCategory = getCategoryById(categoriesCtx.categories, id);
+      if (selectedCategory) {
+        const { name } = selectedCategory;
+        categoriesCtx.deleteCategory(id);
+        set(ref(database, 'categories/' + name), null);
+        set(ref(database, 'services/' + name), null);
+        setShowDeleteModal(false);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Modal show={showDeleteModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This will delete all the services listed under the category as well.
+          Would you like to proceed to delete?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={onDeleteCategoryHandler}>
+            Delete
+          </Button>
+          <Button variant='primary' onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default DeleteModal;
