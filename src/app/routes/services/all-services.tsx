4f09a049b7679c867017a059a6ea01edcf5c9adf
April <huiling_loh_97@hotmail.com>
2023-12-19 20:20:02 +0800

    remove dummy data, fetch categories from firebase

--- src/app/routes/services/all-services.tsx
+++ src/app/routes/services/all-services.tsx
@@ -1,45 +1,15 @@
-import { useEffect, useState } from 'react';
-import { type CardGenericProps } from '../../types/card.ts';
 import CardGrid from '../../components/card/card-grid';
-
-import { onValue, ref } from 'firebase/database';
-import { database } from '../../../main';
+import fetchCategoriesData from '../../util/fetch-categories';
 
 const AllServices = () => {
-  const [categoriesData, setCategoriesData] = useState<CardGenericProps | null>(
-    null
-  );
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const categoriesRef = ref(database, 'categories');
-
-    const fetchData = () => {
-      try {
-        onValue(categoriesRef, (snapshot) => {
-          if (snapshot) {
-            const data = snapshot.val() as CardGenericProps;
-            setCategoriesData(data);
-          } else {
-            throw new Error('Unable to fetch categories');
-          }
-          setIsLoading(false);
-        });
-      } catch (e) {
-        setIsLoading(false);
-        throw new Error('Unable to fetch categories');
-      }
-    };
-
-    fetchData();
-  }, []);
+  const { isLoading, categories } = fetchCategoriesData();
 
   return (
     <div>
-      {isLoading || !categoriesData ? (
+      {isLoading || !categories ? (
         <p>Loading</p>
       ) : (
-        <CardGrid type='generic' cards={categoriesData} />
+        <CardGrid type='generic' cards={categories} />
       )}
     </div>
   );
