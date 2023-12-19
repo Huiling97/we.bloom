874d3dacb050e9e45f28e785baca1ed8008b189c
April <huiling_loh_97@hotmail.com>
2023-12-19 13:12:42 +0800

    fix import meta issue

--- jest.config.ts
+++ jest.config.ts
@@ -7,4 +7,24 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|scss)$': '<rootDir>/src/app/test/__mocks__/styleMock.js',
   },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta', // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+              options: {
+                metaObjectReplacement: { env: 'env123' },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
 };
