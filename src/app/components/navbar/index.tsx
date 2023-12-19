a250910633c9d2f4785f978f651e54a854348437
April <huiling_loh_97@hotmail.com>
2023-12-19 20:20:18 +0800

    added active class for navbar

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import './style.scss'
+import './style.scss';
 
 const TABS_LIST = {
   home: '/',
   services: '/services',
-  contact: '/contact'
-}
+  contact: '/contact',
+};
 
 const NavBar = () => {
   return (
@@ -15,13 +15,15 @@ const NavBar = () => {
         {Object.entries(TABS_LIST).map(([key, value]) => {
           return (
             <li className='navbar-list-item' key={key}>
-              <Link to={`${value}`}>{key}</Link>
+              <NavLink to={`${value}`} className='navbar-link'>
+                {key.toUpperCase()}
+              </NavLink>
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
