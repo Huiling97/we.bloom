b62d58b91b2c73c0f8609264a265d69440c75b38
April <huiling_loh_97@hotmail.com>
2023-12-19 08:49:50 +0800

    use map in contact page

--- src/app/routes/contact.tsx
+++ src/app/routes/contact.tsx
@@ -1,13 +1,17 @@
 import Banner from '../components/banner';
+import Map from '../components/map';
 
 const Contact = () => {
   return (
     <div>
-      <Banner
-        image='/src/app/assets/images/body.jpg'
-        title='Contact Us'
-        description='General enquiries and Booking of appointments '
-      />
+      <div>
+        <Banner
+          image='/src/app/assets/images/body.jpg'
+          title='Contact Us'
+          description='General enquiries and Booking of appointments '
+        />
+      </div>
+      <Map />
     </div>
   );
 };
