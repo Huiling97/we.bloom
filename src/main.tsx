5084cc0cfb8b3faf6c63dd3652ce92a3c0e3725c
April <huiling_loh_97@hotmail.com>
2023-12-21 11:16:47 +0800

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
@@ -21,8 +20,4 @@ const database = getDatabase(app);
 
 export { database };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
