51971cf9de592f5c7abeb6b144592fe0ab4696df
April <huiling_loh_97@hotmail.com>
2023-12-25 15:48:45 +0800

    use react bootstrap navbar and offcanvas

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
-import './style.scss';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { isMobile } from '../../util/screen-size-helper';
 
 const TABS_LIST = {
   home: '/',
@@ -9,23 +13,56 @@ const TABS_LIST = {
 };
 
 const NavBar = () => {
+  const [show, setShow] = useState(false);
+
+  const closeHandler = () => setShow(false);
+  const showHandler = () => setShow(true);
+
+  const expand = isMobile() ? false : true;
+
   return (
-    <div className='navbar'>
-      <ul className='navbar-list'>
-        {Object.entries(TABS_LIST).map(([key, value]) => {
-          return (
-            <li className='navbar-list-item' key={key}>
-              <NavLink
-                to={`${value}`}
-                className='navbar-link link-no-decoration'
-              >
-                {key.toUpperCase()}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <Navbar expand={expand} sticky='top'>
+        <Container fluid>
+          <Navbar.Brand href='#'></Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-false-${expand}`}
+            onClick={showHandler}
+          />
+          <Navbar.Offcanvas
+            show={show}
+            onHide={closeHandler}
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement='end'
+          >
+            <Offcanvas.Header closeButton></Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className='justify-content-end flex-grow-1 pe-3'>
+                <div className='navbar-container'>
+                  {Object.entries(TABS_LIST).map(([key, value]) => {
+                    return (
+                      <div
+                        className='navbar-list-item '
+                        key={key}
+                        onClick={closeHandler}
+                      >
+                        <NavLink
+                          to={`${value}`}
+                          className='navbar-link link-no-decoration'
+                        >
+                          {key.toUpperCase()}
+                        </NavLink>
+                      </div>
+                    );
+                  })}
+                </div>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
