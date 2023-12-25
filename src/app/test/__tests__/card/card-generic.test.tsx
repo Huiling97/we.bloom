66aef78670c7c03a28477832c81ad0bca2c7080c
April <huiling_loh_97@hotmail.com>
2023-12-25 09:50:41 +0800

    update test

--- src/app/test/__tests__/card/card-generic.test.tsx
+++ src/app/test/__tests__/card/card-generic.test.tsx
@@ -1,17 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Router, BrowserRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { cardGenericProps } from '../../__mocks__/card-mock.ts';
 import CardGeneric from '../../../components/card/card-generic/index.tsx';
 
 describe('CardGeneric', () => {
   it('should render component with title', () => {
-    render(<CardGeneric {...cardGenericProps} />);
+    render(
+      <BrowserRouter>
+        <CardGeneric {...cardGenericProps} />
+      </BrowserRouter>
+    );
+
     const title = screen.getByText('MOCK TITLE');
 
     expect(title).toBeInTheDocument();
   });
 
   it('should render component with image', () => {
-    render(<CardGeneric {...cardGenericProps} />);
+    render(
+      <BrowserRouter>
+        <CardGeneric {...cardGenericProps} />
+      </BrowserRouter>
+    );
+
     const image = screen.getByAltText('category image');
 
     expect(image).toBeInTheDocument();
@@ -19,12 +31,34 @@ describe('CardGeneric', () => {
   });
 
   it('should render component with the correct hyperlink', () => {
-    render(<CardGeneric {...cardGenericProps} />);
+    const history = createMemoryHistory();
+    history.push = jest.fn();
+
+    render(
+      <Router location={history.location} navigator={history}>
+        <CardGeneric {...cardGenericProps} />
+      </Router>
+    );
+
     const linkElement = screen.getByRole('link');
 
-    expect(linkElement).toHaveAttribute(
-      'href',
-      `/services/${cardGenericProps.name}`
+    fireEvent.click(linkElement);
+
+    expect(history.push).toHaveBeenCalled();
+    expect(history.push).toHaveBeenCalledWith(
+      {
+        hash: '',
+        pathname: `/services/${cardGenericProps.name}`,
+        search: '',
+      },
+      undefined,
+      {
+        preventScrollReset: undefined,
+        relative: undefined,
+        replace: false,
+        state: undefined,
+        unstable_viewTransition: undefined,
+      }
     );
   });
 });
