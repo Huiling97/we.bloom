48b5412689686ff96cabe8e910346c702a45db90
April <huiling_loh_97@hotmail.com>
2023-12-26 16:52:18 +0800

    export card detailed type

--- src/app/components/card/card-detailed/index.tsx
+++ src/app/components/card/card-detailed/index.tsx
@@ -1,18 +1,11 @@
 import { type ReactNode } from 'react';
 import { type CardDetailsProps } from '../../../types/card.ts';
-
+import { type CardServiceDetailsProps } from '../../../types/card/card-service-details.ts';
 import CardDetails from './details/index.tsx';
-import './style.scss';
-
-export type CardDetailedProps = {
-  name: string;
-  description: string;
-  details: CardDetailsProps[];
-};
 
 const displayCardDetails = (details: CardDetailsProps[]): ReactNode => {
   return (
-    <>
+    <div>
       {details.map(({ duration, price }, index) => (
         <CardDetails
           index={index}
@@ -21,15 +14,21 @@ const displayCardDetails = (details: CardDetailsProps[]): ReactNode => {
           price={price}
         />
       ))}
-    </>
+    </div>
   );
 };
 
-const CardDetailed = ({ name, description, details }: CardDetailedProps) => {
+const CardDetailed = ({
+  name,
+  description,
+  details,
+}: CardServiceDetailsProps) => {
   return (
     <div className='card-detailed-container'>
-      <div className='card-detailed-title'>{name.toUpperCase()}</div>
-      <p className='card-detailed-description'>{description}</p>
+      <div>
+        <div className='card-detailed-title'>{name.toUpperCase()}</div>
+        <p className='card-detailed-description'>{description}</p>
+      </div>
       {details && displayCardDetails(details)}
     </div>
   );
