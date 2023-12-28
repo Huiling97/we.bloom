136c326603f0f900ef890648244fa7d8d2cd5e5a
April <huiling_loh_97@hotmail.com>
2023-12-28 15:30:42 +0800

    move reviews list to constants file

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,41 +1,15 @@
 import { useContext } from 'react';
 import { isEmpty } from 'lodash';
-import LoadingSpinner from '../components/spinner/index.tsx';
 import { Whatsapp } from '@styled-icons/boxicons-logos';
 import { CategoriesContext } from '../store/categories-context.tsx';
+import LoadingSpinner from '../components/spinner/index.tsx';
 import CarouselBanner from '../components/carousel/index.tsx';
 import CardGrid from '../components/card/card-grid/index.tsx';
 import CardReview from '../components/card/card-review/index.tsx';
 import Separator from '../components/separator/index.tsx';
 import fetchCategoriesData from '../util/fetch-categories.ts';
 import { isMobile } from '../util/screen-size-helper.ts';
-
-const REVIEWS_LIST = [
-  {
-    title: 'My skin is finally able to breathe again',
-    description:
-      'I have sensitive skin and the beautician Jean has been very detailed about my facial service, every session was very skilful and relaxing for me. Overall, I received professional services and would recommend Xiu House! ',
-    user: 'April',
-  },
-  {
-    title: 'Absolutely thrilled with my experience at this beauty shop!',
-    description:
-      'The staff was incredibly knowledgeable and friendly, guiding me through a personalized skincare routine that has truly transformed my complexion. The serene atmosphere and top-notch service made me feel pampered and rejuvenated. Highly recommended!',
-    user: 'Joan',
-  },
-  {
-    title: 'Detailed service and kind staffs',
-    description:
-      'I have finally found my hair sanctuary! The hairstylists at this beauty shop are true artists. They took the time to understand my vision and transformed my hair into a masterpiece.',
-    user: 'Madeline',
-  },
-  {
-    title: 'Game changer',
-    description:
-      'The tranquil ambiance and skilled therapists make it my go-to place for relaxation and self-care. The trendy vibe and friendly staff make every visit an enjoyable experience. My go to place for a good service.',
-    user: 'Alicia',
-  },
-];
+import { REVIEWS_LIST } from '../util/constants.ts';
 
 const Home = () => {
   const { categories } = useContext(CategoriesContext);
--- src/app/util/constants.ts
+++ src/app/util/constants.ts
@@ -1,3 +1,30 @@
+export const REVIEWS_LIST = [
+  {
+    title: 'My skin is finally able to breathe again',
+    description:
+      'I have sensitive skin and the beautician Jean has been very detailed about my facial service, every session was very skilful and relaxing for me. Overall, I received professional services and would recommend Xiu House! ',
+    user: 'April',
+  },
+  {
+    title: 'Absolutely thrilled with my experience at this beauty shop!',
+    description:
+      'The staff was incredibly knowledgeable and friendly, guiding me through a personalized skincare routine that has truly transformed my complexion. The serene atmosphere and top-notch service made me feel pampered and rejuvenated. Highly recommended!',
+    user: 'Joan',
+  },
+  {
+    title: 'Detailed service and kind staffs',
+    description:
+      'I have finally found my hair sanctuary! The hairstylists at this beauty shop are true artists. They took the time to understand my vision and transformed my hair into a masterpiece.',
+    user: 'Madeline',
+  },
+  {
+    title: 'Game changer',
+    description:
+      'The tranquil ambiance and skilled therapists make it my go-to place for relaxation and self-care. The trendy vibe and friendly staff make every visit an enjoyable experience. My go to place for a good service.',
+    user: 'Alicia',
+  },
+];
+
 export const PROTECTED_CATEGORY_IDS = [
   '88bbfe85-a9ca-4424-a2b8-3487ae933b5e',
   '0bfb209f-fe46-4959-9841-138c3791aa2d',
