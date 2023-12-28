baacea364a1cfa4408447d4bb4dded33ae846db3
April <huiling_loh_97@hotmail.com>
2023-12-28 13:45:09 +0800

    added protected id for auth helper check

---
+++ src/app/util/constants.ts
@@ -0,0 +1,7 @@
+export const PROTECTED_IDS = [
+  '88bbfe85-a9ca-4424-a2b8-3487ae933b5e',
+  '0bfb209f-fe46-4959-9841-138c3791aa2d',
+  'cb0c9936-488c-4029-bf36-53b90576c29c',
+  '7e8b0a4d-0e57-478b-a97a-c9c07c3d3081',
+  'f016fd72-3dbc-4401-a65c-e35e955f0a06',
+];
