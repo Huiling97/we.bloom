4ed3bc1a857124044165462fe98e86b5a0154946
April <huiling_loh_97@hotmail.com>
2024-01-11 11:21:16 +0800

    added test for routes

---
+++ src/client/app/test/__tests__/components/routes/services/service.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Service from '../../../../../routes/services/service';
+
+describe('Service', () => {
+  it('displays loading spinner while fetching services data', () => {
+    render(<Service />);
+    const loadingSpinner = screen.getByTestId('loading-spinner');
+
+    expect(loadingSpinner).toBeInTheDocument();
+  });
+});
