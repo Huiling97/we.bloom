df04daf5603c24ba56a14b57ccce8f5e293965d2
April <huiling_loh_97@hotmail.com>
2024-01-15 13:43:30 +0800

    update mysql host and add port

--- src/service/products-service.cjs
+++ src/service/products-service.cjs
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.VITE_BASE_API_URL || 'localhost',
+  host: 'localhost' || process.env.VITE_BASE_API_URL,
+  port: 3306,
   user: process.env.VITE_MY_SQL_USER,
   password: process.env.VITE_MY_SQL_PASSWORD,
   database: process.env.VITE_MY_SQL_DATABASE,
