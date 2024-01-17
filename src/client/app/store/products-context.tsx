19e273773bb33c2fb5740ce2e479e65f9f15a37a
April <huiling_loh_97@hotmail.com>
2024-01-17 13:50:57 +0800

    set products in products context

---
+++ src/client/app/store/products-context.tsx
@@ -0,0 +1,44 @@
+import { ReactNode, createContext, useReducer } from 'react';
+import {
+  type ProductsContextProps,
+  type ProductsActionProps,
+} from '../types/context/products';
+import { type ProductProps } from '../types/card/card-product';
+
+const ProductsContext = createContext<ProductsContextProps>({
+  products: [],
+  setProducts: () => {},
+});
+
+const productsReducer = (
+  state: ProductProps[],
+  action: ProductsActionProps
+) => {
+  switch (action.type) {
+    case 'SET':
+      return [...action.payload];
+    default:
+      return state;
+  }
+};
+
+const ProductsContextProvider = ({ children }: { children: ReactNode }) => {
+  const [productsState, dispatch] = useReducer(productsReducer, []);
+
+  const setProducts = (productsData: ProductProps[]) => {
+    dispatch({ type: 'SET', payload: productsData });
+  };
+
+  const value = {
+    products: productsState,
+    setProducts: setProducts,
+  };
+
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+export { ProductsContext, ProductsContextProvider };
