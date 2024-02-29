c0ee7fd36ff81333acc5f0543106f4fb2df79a52
April <huiling_loh_97@hotmail.com>
2024-02-29 09:48:33 +0800

    display cart action items on product page

--- src/client/app/components/card/card-product/index.tsx
+++ src/client/app/components/card/card-product/index.tsx
@@ -1,18 +1,37 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { Button } from 'react-bootstrap';
 import {
   type ProductProps,
   type CardProductProps,
 } from '../../../types/components/card/card-product';
+import { CartContext } from '../../../store/cart-context';
 import { isManageStorePage } from '../../../util/path-helper';
+import { getCartProductQuantity } from './helpers';
 
 const CardProduct = ({ products }: CardProductProps) => {
+  const { cartItems } = useContext(CartContext);
+
   const redirectionUrl = (productId: number) =>
     isManageStorePage()
       ? `/manage/products/${productId}`
       : `/shop/${productId}`;
+
+  const cartActions = (productId: number) => {
+    const cartProductQuantity = getCartProductQuantity(cartItems, productId);
+    if (cartProductQuantity) {
+      return (
+        <>
+          <Button>+</Button>
+          <Button>-</Button>
+        </>
+      );
+    }
+    return <Button>Add to cart</Button>;
+  };
 
   return (
     <Row xs={1} md={4} className='card-product-container'>
@@ -30,6 +49,7 @@ const CardProduct = ({ products }: CardProductProps) => {
                 <Card.Body>
                   <Card.Title>{product.name}</Card.Title>
                   <Card.Text>{product.brand}</Card.Text>
+                  <Card.Text>{cartActions(productId)}</Card.Text>
                 </Card.Body>
               </Link>
             </Card>
