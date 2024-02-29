123a3dde3cfa6cc4e11ba7e48861ddf48a9909a5
April <huiling_loh_97@hotmail.com>
2024-02-29 13:34:39 +0800

    only return cart data if data exists

--- src/client/app/util/fetch-carts-products.ts
+++ src/client/app/util/fetch-carts-products.ts
@@ -3,10 +3,10 @@ import URLConstants from './constants/url-constants';
 
 const fetchCartsProducts = async () => {
   try {
-    const cartProducts = await axios.get(
-      `${URLConstants.CART_PRODUCTS_PATH}/all`
-    );
-    return cartProducts.data;
+    const response = await axios.get(`${URLConstants.CART_PRODUCTS_PATH}/all`);
+    if (response.data) {
+      return response.data;
+    }
   } catch (e) {
     console.error(e);
     throw new Error('Error fetching cart products');
