c3766fa0f6acf3db3a30dd40e3f8fb6678770e81
April <huiling_loh_97@hotmail.com>
2024-02-29 13:34:21 +0800

    update test by mocking axios request

--- src/client/app/test/__tests__/components/routes/home.test.tsx
+++ src/client/app/test/__tests__/components/routes/home.test.tsx
@@ -1,12 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import Home from '../../../../routes/home.tsx';
 import { CategoriesContext } from '../../../../store/categories-context.tsx';
 import { mockCategoriesContextValue } from '../../../__mocks__/store/categories-context-mock.ts';
+import { mockCartContextValue } from '../../../__mocks__/store/cart-context-mock.ts';
+import { CartContext } from '../../../../store/cart-context.tsx';
 
 let container: HTMLElement;
+const mock = new MockAdapter(axios);
 
 describe('Home', () => {
+  beforeAll(() => {
+    // Mock the Axios request for the test
+    mock
+      .onGet('http://localhost:5000/api/v1/carts-products/all')
+      .reply(200, []);
+  });
+
   it('displays loading spinner while fetching categories data', () => {
     render(
       <MemoryRouter>
@@ -23,7 +35,9 @@ describe('Home', () => {
       const { container: renderedContainer } = render(
         <MemoryRouter>
           <CategoriesContext.Provider value={mockCategoriesContextValue}>
-            <Home />
+            <CartContext.Provider value={mockCartContextValue}>
+              <Home />
+            </CartContext.Provider>
           </CategoriesContext.Provider>
         </MemoryRouter>
       );
