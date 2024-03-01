766e17bd830ec1727aab4e664caea49731fd5a3c
April <huiling_loh_97@hotmail.com>
2024-03-01 14:20:48 +0800

    add and update to mysql

--- src/service/carts-service/database.cjs
+++ src/service/carts-service/database.cjs
@@ -1,10 +1,33 @@
 const { db } = require('../databse.cjs');
 
 const getCartProducts = async () => {
-  const q = `SELECT * FROM cart_products`;
+  const q = `SELECT * FROM carts_products`;
 
   const [rows] = await db.query(q);
   return rows;
 };
 
-module.exports = { getCartProducts };
+const addCartProduct = async (productId, price) => {
+  const q = `INSERT INTO carts_products (cart_id, product_id, quantity, total_price) VALUES (?, ?, ?, ?)`;
+  const values = [1, productId, 1, price];
+
+  try {
+    const [result] = await db.query(q, values);
+    return await getCartProducts();
+  } catch (e) {
+    console.error('Error adding item to carts_products in database', e);
+  }
+};
+
+const updateCartProducts = async (productId, quantity, totalPrice) => {
+  const q = `UPDATE carts_products SET quantity = ?, total_price = ? WHERE product_id = ?`;
+
+  try {
+    const [rows] = await db.query(q, [quantity, totalPrice, productId]);
+    return await getCartProducts();
+  } catch (e) {
+    console.error('Error updating carts_products in database', e);
+  }
+};
+
+module.exports = { getCartProducts, addCartProduct, updateCartProducts };
--- src/service/carts-service/index.cjs
+++ src/service/carts-service/index.cjs
@@ -1,4 +1,8 @@
-const { getCartProducts } = require('./database.cjs');
+const {
+  getCartProducts,
+  addCartProduct,
+  updateCartProducts,
+} = require('./database.cjs');
 
 const API_PATH = '/api/v1/carts-products';
 
@@ -6,6 +10,30 @@ const cartsProductsRequestHandler = (app) => {
   app.get(`${API_PATH}/all`, async (req, res) => {
     const cartProducts = await getCartProducts();
     res.status(200).send(cartProducts);
+  });
+
+  app.post(`${API_PATH}/all`, async (req, res) => {
+    const { productId, price } = req.body;
+
+    try {
+      const updatedCart = await addCartProduct(productId, price);
+      res.status(201).send(updatedCart);
+    } catch (e) {
+      return res
+        .status(400)
+        .send({ error: `Error adding product ${productId}` });
+    }
+  });
+
+  app.put(`${API_PATH}/all`, async (req, res) => {
+    const { productId, updatedQuantity, updatedTotalPrice } = req.body;
+
+    const updatedCart = await updateCartProducts(
+      productId,
+      updatedQuantity,
+      updatedTotalPrice
+    );
+    res.status(200).send(updatedCart);
   });
 };
 
